9ae3bd82911a22882086bfab45d2311782d0a6bd
htreb <htrebor20@mail.com>
2025-07-01 10:53:13 -0500

    Update table layout styles for correct size rendering

--- src/modules/models/component/table/utils.tsx
+++ src/modules/models/component/table/utils.tsx
@@ -1,3 +1,4 @@
+import StatusDot from "../../../../components/statusDot";
 import { TypecharacterRick } from "../../../../services/models/types"
 
 export function InfoAppRick(results?: TypecharacterRick["results"]) {
@@ -8,6 +9,14 @@ export function InfoAppRick(results?: TypecharacterRick["results"]) {
     origin: charater.origin.name,
     gender: charater.gender,
     created: charater.created,
-    status: charater.status
+    status: <StatusDot
+      statusDotStyle={
+        charater.status === 'Alive' ? 'successes' :
+          charater.status === 'Dead' ? 'error' :
+            'disabled'
+      }
+      label={charater.status} />
   })) ?? [];
 }
+
+
